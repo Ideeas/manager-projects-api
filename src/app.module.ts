e631e40b7fbbf0dbb87b3d51665897da6593081c
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-11-07 15:27:00 -0300

    importa typeorm module e users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UsersModule],
 })
 export class AppModule {}
