d1da7cd9fa26a65a2d2fa13e6ac17829ce56a4c7
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-11-11 23:37:41 -0300

    adiciona rota que usa o metodo list

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { AddProjectDto } from './dtos/add-project.dto';
 import { Project } from './entities/project.entity';
@@ -6,6 +6,11 @@ import { Project } from './entities/project.entity';
 @Controller('projects')
 export class ProjectsController {
   constructor(private projectsService: ProjectsService) {}
+
+  @Get()
+  async list(): Promise<Project[]> {
+    return await this.projectsService.list();
+  }
 
   @Post('add')
   async add(@Body() body: AddProjectDto): Promise<Project> {
