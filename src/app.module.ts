3df52b8ab724fb81a2c5617fdcd2ae3f68c2e720
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-11-11 23:24:58 -0300

    importa modulo projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
